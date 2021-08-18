e1d2edf60ed462d61c942a1676954dae410f980d
Jaime Muela <jaimuepe@gmail.com>
2021-08-18 19:33:23 +0200

    current user info, sign out

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,25 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentUser', (req, res) => {
-  res.send('hi there');
+
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_KEY!
+    );
+    res.send({ currentUser: payload });
+
+  } catch (err) {
+    return res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
