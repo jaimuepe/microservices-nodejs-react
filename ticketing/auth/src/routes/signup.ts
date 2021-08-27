aaae834564d4d55b45e8ccd35c6209a5ee607e12
Jaime Muela <jaimuepe@gmail.com>
2021-08-27 23:07:56 +0200

    supertest - testing signup service

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -11,7 +11,9 @@ const router = express.Router();
 router.post(
   '/api/users/signup',
   [
-    body('email').isEmail().withMessage('Email must be valid'),
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
     body('password')
       .trim()
       .isLength({ min: 4, max: 20 })
