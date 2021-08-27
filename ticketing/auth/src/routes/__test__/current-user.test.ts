d8335f17a25777d7eb7b2bfe8c434c5042dbc43c
Jaime Muela <jaimuepe@gmail.com>
2021-08-28 01:13:35 +0200

    more tests with supertest

---
+++ ticketing/auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,29 @@
+
+import request from 'supertest';
+
+import { app } from '../../app';
+import { signin } from '../../test/auth-helper';
+
+it('responds with details about the current user', async () => {
+
+  const cookie = await signin();
+
+  const response = await request(app)
+    .get('/api/users/currentUser')
+    .set('Cookie', cookie)
+    .send({})
+    .expect(200);
+
+  expect(response.body.currentUser.email)
+    .toEqual('test@test.com');
+});
+
+it('responds with null if not authenticated', async () => {
+
+  const response = await request(app)
+    .get('/api/users/currentUser')
+    .send({})
+    .expect(200);
+
+  expect(response.body.currentUser).toBeNull();
+});
